6484d58926b676112b5dbe5a7db4f3d302600324
unknown <auchiyam1994@gmail.com>
2019-03-09 14:08:55 -0600

    removed express.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-    entry: './src/typescript/game_engine/main',
+    entry: './src/typescript/game_engine/main.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
