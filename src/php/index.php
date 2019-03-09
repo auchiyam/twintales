1f473f9ed157f2e14873b5fb238d6e57a4376b26
unknown <auchiyam1994@gmail.com>
2019-03-09 17:38:55 -0600

    began get_assets functionality

--- src/php/index.php
+++ src/php/index.php
@@ -1,7 +1,33 @@
 <!DOCTYPE html>
 
 <html>
-    <php?
-        echo "Hello World!";
+    <?php
+        // since it's impossible to access mysql remotely, just use the user/pass of mysql here
+        // also ensure that this user can only read the tables
+        $server = "localhost";
+        $user = "reader";
+        $pass = "password";
+
+        // get the function and arguments
+        $func = $_GET['func'];
+        $args = $_GET['args'];
+
+        // create connection to the 
+        try {
+            $conn = new PDO("mysql:host=$server;dbname=twintales", $user, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connection success";
+        }
+        catch (PDOException $e) {
+            echo "connection failed: \n" . $e->getMessage();
+        }
+
+        if ($func === 'get_assets') {
+            echo "hey this is a test"
+        }
+
+        if ($func === 'get_ranking') {
+
+        }
     ?>
 </html>
