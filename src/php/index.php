2aa37ff2cff2cfbf6fc3e77447203c83ac100c37
unknown <auchiyam1994@gmail.com>
2019-03-09 20:29:08 -0600

    adjusted php to adhere to the new api format

--- src/php/index.php
+++ src/php/index.php
@@ -1,15 +1,17 @@
 <?php
+// resource for creating API in php: https://www.codeofaninja.com/2017/02/create-simple-rest-api-in-php.html
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
 
     // since it's impossible to access mysql remotely, just put the user/pass of mysql here
-    // also ensure that this user can only read the tables
+    // also ensure that this user can only read the tables for double safety
     $server = "localhost";
     $user = "reader";
     $pass = "password";
 
     // get the function and arguments
-    extract($_GET);
+    $func = $_GET['func'];
+    extract(json_decode($_GET['args'], true));
 
     // create connection to the mysql
     $conn = null;
@@ -23,7 +25,7 @@
 
     // get all the assets for the given argument
     if ($func === 'get_assets') {
-        // execute the mysql 
+        // execute the mysql query
         $sql = "select * from asset where asset.asset_type=\"". $a_type . "\" and asset.file_type=\"" . $f_type . "\"";
 
         $statement = $conn->prepare($sql);
@@ -32,28 +34,27 @@
 
         $count = $statement->rowCount();
 
-        // found some in the table
-        if ($count > 0) {
-            $json_value = array();
-            $json_value['count'] = $count;
+        // create the array that will be used to make json with
+        $json_value = array();
+        $json_value['count'] = $count;
 
-            // loop all the contents
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                // extract everything
-                extract($row);
+        // loop all the contents
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            // extract info
+            extract($row);
 
-                $asset = array(
-                    "name" => $name,
-                    "location" => $location
-                );
+            $asset = array(
+                "name" => $name,
+                "location" => $location
+            );
 
-                array_push($json_value, $asset);
-            }
-
-            http_response_code(200);
-
-            echo json_encode($json_value);
+            array_push($json_value, $asset);
         }
+
+        // respond with success code and return the json value
+        http_response_code(200);
+
+        echo json_encode($json_value);
     }
 
     // get the ranking
